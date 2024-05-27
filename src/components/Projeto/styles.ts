32cf48fd4793ac920451449fa2af3eb0760faf07
Bruno <bdorigon@hotmail.com>
2024-05-27 11:34:34 -0300

    criando a seção de projetos e componente de projetos

---
+++ src/components/Projeto/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+  border: 1px solid #c1c1c1;
+  padding: 16px;
+`;
+
+export const LinkBotao = styled.a`
+  font-size: 14px;
+  color: #fff;
+  background: #4476bf;
+  padding: 8px;
+  cursor: pointer;
+  display: inline-block;
+`
