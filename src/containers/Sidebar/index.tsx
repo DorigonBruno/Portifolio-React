34413acab2ba8baa3cbfac5ce07de3109feab48e
Bruno <bdorigon@hotmail.com>
2024-05-27 11:57:29 -0300

    estilizando a side bar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,10 +1,20 @@
+import Avatar from "../../components/Avatar";
+import Paragrafo from "../../components/Paragrafo";
 import Titulo from "../../components/Titulo";
+import { BotaoTema, SideBarContainer } from "./styles";
 
 const Sidebar = () => {
   return (
     <aside>
-      <img src="https://github.com/DorigonBruno.png" alt="foto do github" />
-      <Titulo fontSize={18}>Bruno Dorigon</Titulo>
+      <SideBarContainer>
+        <Avatar />
+        <Titulo fontSize={18}>Bruno Dorigon</Titulo>
+        <Paragrafo tipo="secundario" fontSize={16}>
+          DorigonBruno
+        </Paragrafo>
+        <Paragrafo fontSize={14}>Desenvolvedor Front-End</Paragrafo>
+        <BotaoTema>Trocar Tema</BotaoTema>
+      </SideBarContainer>
     </aside>
   );
 };
---
+++ src/containers/Sidebar/styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const BotaoTema = styled.button`
+  background: #282a35;
+  color: #eee;
+  font-size: 12px;
+  border-radius: 12px;
+  padding: 8px;
+  font-weight: 700;
+  border: 2px solid #282a35;
+  cursor: pointer;
+`
+
+export const SideBarContainer = styled.div`
+  position: sticky;
+  top: 80px;
+  left: 0;
+`
